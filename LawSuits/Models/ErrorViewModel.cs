a86c9e89ee88b5eda2a24f8fe6eba51e7d9c88cc
ni-sgn <ni.saganeli@gmail.com>
2021-01-10 21:25:54 +0400

    Template Project

---
+++ LawSuits/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LawSuits.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
